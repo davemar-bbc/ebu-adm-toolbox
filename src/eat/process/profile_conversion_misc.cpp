93c18dcde754618024a2e2237002c1e9d36bd08d
davemar-bbc <david.marston@bbc.co.uk>
2025-12-18 23:07:28 +0000

    Updated profile_conversion_misc.cpp to have correct profile names and header.

--- src/eat/process/profile_conversion_misc.cpp
+++ src/eat/process/profile_conversion_misc.cpp
@@ -17,14 +17,14 @@ namespace eat::process {
 
 struct ToAdmProfile {
   adm::Profile operator()(profiles::ITUEmissionProfile &p) {
-    return adm::Profile{adm::ProfileValue{"ITU-R BS.[ADM-NGA-Emission]-0"},
-                        adm::ProfileName{"AdvSS Emission ADM and S-ADM Profile"}, adm::ProfileVersion{"1"},
+    return adm::Profile{adm::ProfileValue{"ITU-R BS.2168"},
+                        adm::ProfileName{"Advanced sound system: ADM and S-ADM profile for emission"}, adm::ProfileVersion{"1"},
                         adm::ProfileLevel{std::to_string(p.level())}};
   }
 
   adm::Profile operator()(profiles::EBUProductionProfile &p) {
     (void)p;
-    return adm::Profile{adm::ProfileValue{"EBU Tech XXXX"},
+    return adm::Profile{adm::ProfileValue{"EBU Tech 3393"},
                         adm::ProfileName{"EBU Production Profile"}, adm::ProfileVersion{"1"},
                         adm::ProfileLevel{std::to_string(0)}};
   }
@@ -45,9 +45,11 @@ class SetProfiles : public FunctionalAtomicProcess {
     std::vector<adm::Profile> adm_profiles;
     for (auto &profile : profiles) adm_profiles.push_back(std::visit(ToAdmProfile(), profile));
 
-    auto profile_list = adm::ProfileList{std::move(adm_profiles)};
-    //doc->set(profile_list);
-    //doc->set(adm::ProfileList{std::move(adm_profiles)});
+    auto profile_list = std::make_shared<adm::ProfileList>();
+    for (auto &adm_profile : adm_profiles) {
+      profile_list->add(adm_profile);
+    }
+    doc->add(profile_list);
 
     adm.document = std::move(doc);
     out_axml->set_value(std::move(adm));
